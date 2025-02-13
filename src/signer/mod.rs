b6ac22d47010ee1d081747b6ad7595bc41a00866
incubator4 <aries0robin@gmail.com>
2025-02-13 20:12:28 +0800

    chore: remove commented-out test code from signer module

--- src/signer/mod.rs
+++ src/signer/mod.rs
@@ -62,11 +62,5 @@ impl SignerConfig {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-
-    // #[test]
-    // fn test_rlp() {
-    //     let rlp = "0xa3f20717a250c2b0b729b7e5becbff67fdaef7e0699da4de7ca5895b02a170a12d887fd3b17bfdce3481f10bea41f45ba9f709d39ce8325427b57afcfc994cee1b";
-    //     let tx = TypedTransaction::
-    // }
+    // use super::*;
 }
